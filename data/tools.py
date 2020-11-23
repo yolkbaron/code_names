ec59d7969d73624a83ddcf78e56008ad251e32f8
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-11-23 12:20:13 +0300

    added some tools

---
+++ data/tools.py
@@ -0,0 +1,9 @@
+def load_all_words(directory, extensions = []):
+    words = []
+    pass
+def load_all_sounds(directory, extensions = []):
+    sounds = []
+    pass
+def load_all_sprites(directory, extensions = []):
+    sprites = []
+    pass
