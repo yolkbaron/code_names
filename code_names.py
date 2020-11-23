5ae34a989d72d5e29c9d0d586204e0b1c9df455f
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-11-23 12:13:59 +0300

    added main .py file

---
+++ code_names.py
@@ -0,0 +1,12 @@
+"""
+This is Code Names game recreated using python
+"""
+
+import sys
+import pygame as pg
+from data.main import main
+
+if __name__=='__main__':
+    main()
+    pg.quit()
+    sys.exit()
