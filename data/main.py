cb2cb2a98bfda6aea0c68e08208466eddd2162c4
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-11-28 00:20:24 +0300

    Completed main file

--- data/main.py
+++ data/main.py
@@ -1,2 +1,14 @@
+from . import setup, tools
+from .screens import field_operative_screen, main_menu, spymaster_screen, starting_screen
+from . import constants as c
+
+
 def main():
-    pass
+    game = tools.Game(c.BASIC_CAPTION)
+    screen_dict = {c.FIELD_OPERATIVE: field_operative_screen.FiledOperative,
+                   c.MAIN_MENU: main_menu.MainMenu,
+                   c.SPYMASTER: spymaster_screen.SpyMaster,
+                   c.STARTING_SCREEN: starting_screen.StartingScreen
+                   }
+    game.set_screens(screen_dict, c.STARTING_SCREEN)
+    game.main()
