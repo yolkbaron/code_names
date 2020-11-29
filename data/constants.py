d951287b0e24a5da836d1eb0a393e55d07916947
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-11-29 21:42:37 +0300

    PEP8

--- data/constants.py
+++ data/constants.py
@@ -12,47 +12,47 @@ ALLOWED_EVENTS = [pg.MOUSEBUTTONUP, pg.MOUSEBUTTONDOWN, pg.KEYUP, pg.KEYDOWN, pg
 BASIC_CAPTION = 'Code Names'
 MENU_CAPTION = 'Main Menu'
 
-WORD_CATEGORIES = ["words_list"] #TODO
+WORD_CATEGORIES = ["words_list"]  # TODO should be chosen on the starting screen
 
-## COLORS ##
+# Colors
 
 #               R    G    B
 GRAY         = (100, 100, 100)
-NAVYBLUE     = ( 60,  60, 100)
+NAVYBLUE     = (60,  60,  100)
 WHITE        = (255, 255, 255)
-RED          = (255,   0,   0)
-GREEN        = (  0, 255,   0)
-FOREST_GREEN = ( 31, 162,  35)
-BLUE         = (  0,   0, 255)
-SKY_BLUE     = ( 39, 145, 251)
-YELLOW       = (255, 255,   0)
-ORANGE       = (255, 128,   0)
-PURPLE       = (255,   0, 255)
-CYAN         = (  0, 255, 255)
-BLACK        = (  0,   0,   0)
-NEAR_BLACK    = ( 19,  15,  48)
+RED          = (255, 0,   0)
+GREEN        = (0,   255, 0)
+FOREST_GREEN = (31,  162, 35)
+BLUE         = (0,   0,   255)
+SKY_BLUE     = (39,  145, 251)
+YELLOW       = (255, 255, 0)
+ORANGE       = (255, 128, 0)
+PURPLE       = (255, 0, 255)
+CYAN         = (0,   255, 255)
+BLACK        = (0,   0,   0)
+NEAR_BLACK   = (19,  15,  48)
 COMBLUE      = (233, 232, 255)
-GOLD         = (255, 215,   0)
+GOLD         = (255, 215, 0)
 
 BGCOLOR = WHITE
 
-#WordCard states
+# WordCard states
 COVERED = 'covered'
 UNCOVERED = 'uncovered'
 
-#Move states
+# Move states
 TEAM1 = '1 team'
 TEAM2 = '2 team'
 
-#Screen states
+# Screen states
 MAIN_MENU = 'main menu'
 FIELD_OPERATIVE = 'field operative'
 SPYMASTER = 'spymaster'
 STARTING_SCREEN = 'starting screen'
 
-#Game info dictionary keys
+# Game info dictionary keys
 TEAM1_SCORE = '1 team score'
 TEAM2_SCORE = '2 team score'
 
-#Sound states
-#TODO
+# Sound states
+# TODO
