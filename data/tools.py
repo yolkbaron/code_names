398f8fde76f26f5e19a6801b81d29ad499768624
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-11-29 17:57:51 +0300

    Added descriptions to load functions

--- data/tools.py
+++ data/tools.py
@@ -7,7 +7,8 @@ class Game(object):
     """
     Class for handling entire project. Contains game, event and main loops.
     """
-    #TODO
+
+    # TODO
     def __init__(self, caption):
         self.caption = caption
         self.screen = pg.display.get_surface()
@@ -43,6 +44,7 @@ class Game(object):
         """
         # TODO
         pass
+
     def set_screens(self, screen_dict, current_screen_name):
         """
         Sets self.screen_dict, self.screen_name, self.screen with data given
@@ -50,7 +52,7 @@ class Game(object):
         :param current_state: starting state
         :return: None
         """
-        #TODO
+        # TODO
         pass
 
 
@@ -58,24 +60,56 @@ class GameState(object):
     """
     Contains current previous and next states of Game
     """
-    #TODO
+
+    # TODO
     def __init__(self):
         self.done = False
 
 
-def load_all_words(directory, extensions=()):
+def load_all_words(directory, categories, extensions=()):
+    """
+    Loads all words of given categories from given directory.
+    :param directory: Directory with words
+    :param categories: Allowed categories for words
+    :param extensions: Allowed extensions os files with words
+    :return: Lisr of words
+    """
     # TODO
     words = []
     pass
 
 
 def load_all_sounds(directory, extensions=()):
+    """
+    Loads all sounds from given directory
+    :param directory: Directory with sounds
+    :param extensions: Allowed extensions for sound files
+    :return: List of sounds
+    """
     # TODO
     sounds = []
     pass
 
 
+def load_all_music(directory, extensions=()):
+    """
+    Loads all music from given directory
+    :param directory: Directory with music
+    :param extensions: Allowed extensions for music files
+    :return: List of music
+    """
+    # TODO
+    music = []
+    pass
+
+
 def load_all_sprites(directory, extensions=()):
+    """
+    Loads all sprites from given directory
+    :param directory: Directory with sprites
+    :param extensions: Allowed extensions for sprite files
+    :return: List of sprites
+    """
     # TODO
     sprites = []
     pass
