94ab54789b957cf334e92bcb5caf6649d3ff0373
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-11-29 17:56:51 +0300

    Completed setup module

--- data/setup.py
+++ data/setup.py
@@ -1,5 +1,19 @@
 """
-Initializes the game screen and creates dictionaries of sounds and sprites, list of word
+Initializes the game screen and creates dictionaries of sounds, sprites and music, list of words
 """
-# TODO
-pass
+
+import os
+import pygame as pg
+from . import tools
+from. import constants as c
+
+pg.init()
+pg.event.set_allowed(c.ALLOWED_EVENTS)
+pg.display.set_caption(c.BASIC_CAPTION)
+SCREEN = pg.display.set_mode(c.SCREEN_SIZE)
+SCREEN_RECT = SCREEN.get_rect()
+
+SPRITES = tools.load_all_sprites(os.path.join("resources", "sprites"))
+SOUNDS = tools.load_all_sprites(os.path.join("resources", "sounds"))
+MUSIC = tools.load_all_sprites(os.path.join("resources", "music"))
+WORDS = tools.load_all_sprites(os.path.join("resources", "words"), c.WORD_CATEGORIES)
