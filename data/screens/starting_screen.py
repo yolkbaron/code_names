a4fe61fb552879b4baa2d64ac80f596fb3ac4702
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-12-08 12:34:36 +0300

    FIXME

--- data/screens/starting_screen.py
+++ data/screens/starting_screen.py
@@ -7,17 +7,17 @@ class StartingScreen(tools.GameState):
 
     def __init__(self):
         tools.GameState.__init__(self)
-        self.screen = pg.display.set_mode((1000, 1000))
+        self.screen = pg.display.set_mode((1000, 1000))  # FIXME screen size is in constants
         f1 = pg.font.Font(None, 80)
         f2 = pg.font.Font(None, 50)
-        text1 = f1.render('Приветствую вас странники!', 1, c.RED)
-        text2 = f2.render('Если ты хотел сыграть в CodeNames,', 1, c.RED)
-        text3 = f2.render('то ты попал прямо по адресу.', 1, c.RED)
-        text4 = f2.render('Для начала игры ознакомтесь с инсрукцией.', 1, c.RED)
-        text5 = f2.render('Теперь смело жми на старт!', 1, c.RED)
-        self.screen.blit(text1, (120, 100))
-        self.screen.blit(text2, (200, 200))
-        self.screen.blit(text3, (260, 250))
-        self.screen.blit(text4, (130, 500))
-        self.screen.blit(text5, (260, 550))
+        text1 = f1.render('Приветствую вас странники!', True, c.RED)
+        text2 = f2.render('Если ты хотел сыграть в CodeNames,', True, c.RED)
+        text3 = f2.render('то ты попал прямо по адресу.', True, c.RED)
+        text4 = f2.render('Для начала игры ознакомтесь с инсрукцией.', True, c.RED)
+        text5 = f2.render('Теперь смело жми на старт!', True, c.RED)
+        self.screen.blit(text1, (120, 100))  # FIXME
+        self.screen.blit(text2, (200, 200))  # FIXME
+        self.screen.blit(text3, (260, 250))  # FIXME
+        self.screen.blit(text4, (130, 500))  # FIXME
+        self.screen.blit(text5, (260, 550))  # FIXME
 
