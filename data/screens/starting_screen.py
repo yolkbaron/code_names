56e44f17e3209c56a845334f256c66bdd99a1ebd
Daniil Sakahrov <71354158+yolkbaron@users.noreply.github.com>
2020-12-18 18:16:30 +0300

    Starting screen almost finished

--- data/screens/starting_screen.py
+++ data/screens/starting_screen.py
@@ -35,25 +35,25 @@ class StartingScreen(tools.GameState):
 
     def set_buttons(self):
         start_button = button.Button(
-            int(160 * self.multiplier),
-            int(870 * self.multiplier),
+            int(180 * self.multiplier),
+            int(820 * self.multiplier),
             int(600 * self.multiplier),
             int(200 * self.multiplier),
             "START",
             int(150 * self.multiplier),
-            "top secret text"
+            "Top Secret"
         )
         start_button.set_inactive(c.GRAY)
         start_button.set_active(c.RED)
         self.buttons["start"] = start_button
 
     def set_textboxes(self):
-        spy1 = text_box.InputBox(220, 435, 500, 100, c.WHITE, 50, "top secret text")
-        spy2 = text_box.InputBox(1100, 250, 500, 100, c.WHITE, 50, "top secret text")
-        team1_operative1 = text_box.InputBox(220, 555, 500, 100, c.WHITE, 50, "top secret text")
-        team2_operative1 = text_box.InputBox(1100, 370, 500, 100, c.WHITE, 50, "top secret text")
-        team1_operative2 = text_box.InputBox(220, 675, 500, 100, c.WHITE, 50, "top secret text")
-        team2_operative2 = text_box.InputBox(1100, 490, 500, 100, c.WHITE, 50, "top secret text")
+        spy1 = text_box.InputBox(220, 435, 500, 100, c.BLUE, 100, "top secret text")
+        spy2 = text_box.InputBox(1100, 250, 500, 100, c.RED, 100, "top secret text")
+        team1_operative1 = text_box.InputBox(220, 555, 500, 100, c.BLUE, 100, "top secret text")
+        team2_operative1 = text_box.InputBox(1100, 370, 500, 100, c.RED, 100, "top secret text")
+        team1_operative2 = text_box.InputBox(220, 675, 500, 100, c.BLUE, 100, "top secret text")
+        team2_operative2 = text_box.InputBox(1100, 490, 500, 100, c.RED, 100, "top secret text")
         self.text_boxes["team1_spy"] = spy1
         self.text_boxes["team2_spy"] = spy2
         self.text_boxes["team1_operative1"] = team1_operative1
